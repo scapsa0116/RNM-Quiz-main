40c5b0f3478ba70f112377a9ae1d6ccd220fc279
stela <stela.capsa@gmail.com>
2025-10-06 15:56:07 -0500

    buttons

---
+++ src/components/AnswerOption.tsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from "react-native";
+//Responsible for each answer option
+
+
+
+export default function AnswerOption(){
+
+
+    return (
+        <View style = {styles.answer}>
+            <Text>Answer Option</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+ answer:{
+    borderWidth:1,
+    borderColor: '#0d0c0cff',
+    borderRadius: 20,
+    padding:20,
+    
+    
+
+ }
+})
+
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet } from "react-native"
+import AnswerOption from "./AnswerOption"
 
 //this component is about the container of the questions
 
@@ -8,6 +9,12 @@ export default function QuestionCard (){
     return(
         <View style ={styles.container}>
             <Text style = {styles.question}>What is React Native?</Text>
+            <View style = {styles.answer}>
+                <View style = {styles.answer}><AnswerOption/></View>
+                <View style = {styles.answer}><AnswerOption/></View>
+                <View style = {styles.answer}><AnswerOption/></View>
+                <View style = {styles.answer}><AnswerOption/></View>
+            </View>
         </View>
     )
 }
@@ -19,17 +26,33 @@ export default function QuestionCard (){
 const styles = StyleSheet.create({
 
 container: {
-backgroundColor: 'red',
-padding: 20,
-paddingLeft: 100,
+backgroundColor: '#f5eeeeff',
+padding:20,
 borderRadius: 20,
 height: 450,
 width: 350,
+alignItems: 'center',
+shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 6,
+},
+shadowOpacity: 0.39,
+shadowRadius: 8.30,
+
+elevation: 13,
 },
  question:{
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: '500',
     
- }
+ },
+
+answer:{
+   padding: 10,
+width: 300,
+//    height:40,
+
+}
 
 })
